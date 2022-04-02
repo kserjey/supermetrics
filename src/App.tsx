7ae5923444a7f02b3b550f2067ddd86771bfa154
Sergei Korchinskii <k.serjey@gmail.com>
2022-04-03 01:05:44 +0300

    feat: login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import axios, { AxiosPromise } from 'axios';
+
+interface LoginData {
+  name: string;
+  email: string;
+}
+
+interface TokenData {
+  sl_token: string;
+  client_id: string;
+  email: string;
+}
+
+function registerToken(data: LoginData): AxiosPromise<{ data: TokenData }> {
+  return axios.post('https://api.supermetrics.com/assignment/register', {
+    ...data,
+    client_id: 'ju16a6m81mhid5ue1z3v2g0uh',
+  });
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const loginData = Object.fromEntries(formData) as unknown as LoginData;
+          registerToken(loginData).then(({ data }) => {
+            localStorage.setItem('token', data.data.sl_token);
+          });
+        }}
+      >
+        <label htmlFor="nameInput">
+          Name
+          <input name="name" id="nameInput" />
+        </label>
+        <label htmlFor="emailInput">
+          E-mail
+          <input name="email" id="emailInput" />
+        </label>
+        <button type="submit">Go</button>
+      </form>
     </div>
   );
 }
